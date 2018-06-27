0038a599f600e5d91699bd00fd5ecea7fd790e48
HomerAria <zhouxuan_zx@hotmail.com>
2018-06-27 16:55:56 +0800

    change test

--- app/src/main/java/com/sean/test/test/MainActivity.java
+++ app/src/main/java/com/sean/test/test/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        //发生改变
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
